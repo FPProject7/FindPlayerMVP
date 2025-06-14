53f634484e32e6ab5f5354542d085c7c95a13790
JadEid99 <jad.n.eid@gmail.com>
2025-06-14 11:17:52 +0200

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="p-8">
+      <h1 className="text-3xl font-bold underline text-blue-600">
+        FindPlayer MVP Setup Complete!
+      </h1>
+    </div>
+  )
+}
+
+export default App
