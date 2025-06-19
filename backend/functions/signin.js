bf88eef81cdc7207a0cbf3aa1cd017123563d65a
JadEid99 <jad.n.eid@gmail.com>
2025-06-19 13:39:00 +0200

    staged all changes

--- backend/functions/signin.js
+++ backend/functions/signin.js
@@ -1,15 +1,14 @@
 // backend/functions/signin.js
 
 import { CognitoIdentityProviderClient, InitiateAuthCommand } from "@aws-sdk/client-cognito-identity-provider";
-
-// --- FINAL CONFIGURATION ---
 const REGION = "us-east-1";
-const CLIENT_ID = "3bn5bf8c9ks2rpu9q7jkefhcni";
+const CLIENT_ID = "29ae68avp4t8mvcg30fr97j3o2";
 
 const client = new CognitoIdentityProviderClient({ region: REGION });
 
 export const handler = async (event) => {
     const { email, password } = JSON.parse(event.body);
+
 
     const params = {
         AuthFlow: "USER_PASSWORD_AUTH",
@@ -18,23 +17,19 @@ export const handler = async (event) => {
             USERNAME: email,
             PASSWORD: password,
         },
-        // SecretHash logic is completely removed
     };
 
     try {
         const command = new InitiateAuthCommand(params);
         const { AuthenticationResult } = await client.send(command);
-
-        if (AuthenticationResult) {
-            return {
-                statusCode: 200,
-                headers: { "Access-Control-Allow-Origin": "*" },
-                body: JSON.stringify({
-                    message: "User signed in successfully",
-                    tokens: AuthenticationResult,
-                }),
-            };
-        }
+        return {
+            statusCode: 200,
+            headers: { "Access-Control-Allow-Origin": "*" },
+            body: JSON.stringify({
+                message: "User signed in successfully",
+                tokens: AuthenticationResult,
+            }),
+        };
     } catch (error) {
         console.error(error);
         return {
--- backend/functions/signup.js
+++ backend/functions/signup.js
@@ -4,7 +4,7 @@ import { CognitoIdentityProviderClient, SignUpCommand } from "@aws-sdk/client-co
 import { createHmac } from 'crypto';
 
 const REGION = "us-east-1";
-const CLIENT_ID = "3bn5bf8c9ks2rpu9q7jkefhcni";
+const CLIENT_ID = "29ae68avp4t8mvcg30fr97j3o2";
 
 const client = new CognitoIdentityProviderClient({ region: REGION });
 
